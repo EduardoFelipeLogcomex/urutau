25b219c16b5846f16d5d6cd3d396e71820856c08
Silva, Eduardo Cipriano <eduardo.felipe@logcomex.com>
2024-04-19 11:55:42 -0300

    add feature initial files

---
+++ src/components/product/urt-product-navigator/urt-product-navigator.template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="urt__nav">
+    <div class="urt__nav__sec">
+      <img src="@/assets/img/logcomex-logo-full.png" alt="logcomex company logo" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="sass">
+@import "./urt-product-navigator.sass"
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
   plugins: [vue()],
   build: {
     lib: {
